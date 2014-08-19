89904c24565093d81df7672f5bf89e0e993bbbff
Brandon <brandon-adams@users.noreply.github.com>
2014-08-19 11:48:03 -0400

    Update jenkins_deploy.sh

--- jenkins_deploy.sh
+++ jenkins_deploy.sh
@@ -6,4 +6,4 @@ cf login -a http://api.192.168.4.14.xip.io -u admin -p password -o newwave -s de
 APP_NAME=`cat manifest.yml | grep -i name | awk '{ print $3 }'`
 #cf restage $APP_NAME
 #cf start $APP_NAME
-sudo /opt/IBM/WebSphere/AppServer/profiles/eMoYx3-dmgr/bin/wsadmin.sh -lang jython -f /home/opstack/deployWAR.py eMoYx3-mngd-AppNode GnOEpf-srvr $WORKSPACE/build/libs/activitysession.ear
+sudo /opt/IBM/WebSphere/AppServer/profiles/eMoYx3-dmgr/bin/wsadmin.sh -lang jython -f /home/opstack/deployWAR.py eMoYx3-mngd-AppNode GnOEpf-srvr $WORKSPACE/build/libs/helloworld.war
