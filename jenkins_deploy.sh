f5f20ea70fd1ce66209bbffbdaf0938cd5d800a5
Brandon <brandon-adams@users.noreply.github.com>
2014-08-19 11:25:10 -0400

    Update jenkins_deploy.sh

--- jenkins_deploy.sh
+++ jenkins_deploy.sh
@@ -2,8 +2,8 @@
 
 bash $WORKSPACE/gradlew build
 cf login -a http://api.192.168.4.14.xip.io -u admin -p password -o newwave -s dev
-cf push --no-start
+#cf push --no-start
 APP_NAME=`cat manifest.yml | grep -i name | awk '{ print $3 }'`
 #cf restage $APP_NAME
-#f start $APP_NAME
-#sudo /opt/IBM/WebSphere/AppServer/bin/wsadmin.sh -lang jython -f /home/opstack/deployWAR.py eMoYx3-mngd-AppNode GnOEpf-srvr $WORKSPACE/build/libs/helloworld.war
+#cf start $APP_NAME
+sudo /opt/IBM/WebSphere/AppServer/bin/wsadmin.sh -lang jython -f /home/opstack/deployWAR.py eMoYx3-mngd-AppNode GnOEpf-srvr $WORKSPACE/build/libs/helloworld.war
