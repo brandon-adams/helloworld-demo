37863909552f2105877df06cea1bd6ed61bfbb6c
Brandon <brandon-adams@users.noreply.github.com>
2014-08-19 17:08:22 -0400

    Update jenkins_deploy.sh

--- jenkins_deploy.sh
+++ jenkins_deploy.sh
@@ -2,7 +2,7 @@
 
 bash $WORKSPACE/gradlew build
 cf login -a http://api.192.168.4.14.xip.io -u admin -p password -o newwave -s dev
-#cf push --no-start
+cf push --no-start
 APP_NAME=`cat manifest.yml | grep -i name | awk '{ print $3 }'`
 #cf restage $APP_NAME
 #cf start $APP_NAME
